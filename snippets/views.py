c9096776dc244b474965e0ccb3e6df8902429e6c
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-25 19:29:28 -0300

    refactor: change classes even further to use DjangoRESTful generic views

--- snippets/views.py
+++ snippets/views.py
@@ -8,9 +8,7 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(
-    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
-):
+class SnippetList(generics.ListCreateAPIView):
     """
     List all snippets, or create a new snippet.
     """
@@ -18,31 +16,11 @@ class SnippetList(
     queryset = Snippet.objects.add()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
-):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
