e35ed17f5083c67ec3e6a9b0dbb9f62a291a67a3
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-27 10:58:27 -0300

    fix: remove all not user method from views.py that were conflicting

--- snippets/views.py
+++ snippets/views.py
@@ -1,22 +1,11 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import action
-from rest_framework.reverse import reverse
 from rest_framework.response import Response
 
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
-
-
-@api_view(["GET"])
-def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "snippets": reverse("snippet-list", request=request, format=format),
-        }
-    )
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
